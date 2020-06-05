8b29ab4a0e1c62a64e5aca9f0b3ad6d149451d6f
Carsten Götzinger <carsten.goetzinger@concedro.com>
2020-06-05 21:31:55 +0200

    Prepare for next version

--- travis-build.sh
+++ travis-build.sh
@@ -9,16 +9,17 @@ rm -rf *.zip
 filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
+VERSION=$(grep -e "projectVersion" gradle.properties | sed 's/projectVersion=//')
+
 echo "branch: $TRAVIS_BRANCH"
 echo "pullrequest: $TRAVIS_PULL_REQUEST"
 echo "travis tag: $TRAVIS_TAG"
+echo "version: $VERSION"
 
 EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
-if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
+if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]] && [[ ! -z $(echo $VERSION | grep "SNAPSHOT") ]]; then
 
-  echo "Publishing archives"
-
+  echo "Publishing archives for branch $TRAVIS_BRANCH"
   if [[ -n $TRAVIS_TAG ]]; then
       ./gradlew bintrayUpload || EXIT_STATUS=$?
   else
